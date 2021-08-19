8ee415c7176c426bd05435263ccc8e31d3f09bf4
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2021-08-19 17:19:18 +0000

    stage five completion

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,5 +1,6 @@
 # Write your code here
 
+
 # State of the coffee machine
 AMOUNT_OF_WATER = 400
 AMOUNT_OF_MILK = 540
@@ -8,10 +9,63 @@ AMOUNT_OF_DISPOSABLE_CUPS = 9
 COFFEE_MACHINE_INCOME = 550
 
 
-def coffee_machine():
-    state_of_coffee_machine()
-    print()
-    coffee_machine_operation()
+# def coffee_machine():
+#    coffee_machine_operation(input("Write action (buy, fill, take, remaining, exit):"))
+
+def check_coffee_resources_and_act(user_input):
+    global AMOUNT_OF_WATER
+    global AMOUNT_OF_COFFEE_BEANS
+    global COFFEE_MACHINE_INCOME
+    global AMOUNT_OF_MILK
+    global AMOUNT_OF_DISPOSABLE_CUPS
+
+    if user_input == 1:
+        if AMOUNT_OF_WATER < 250:
+            print("Sorry, not enough water!")
+        elif AMOUNT_OF_COFFEE_BEANS < 16:
+            print("Sorry, not enough coffee beans!")
+        elif AMOUNT_OF_DISPOSABLE_CUPS < 1:
+            print("Sorry, not enough disposable cups!")
+        else:
+            print("I have enough resources, making you a coffee!")
+            AMOUNT_OF_WATER = AMOUNT_OF_WATER - 250
+            AMOUNT_OF_COFFEE_BEANS = AMOUNT_OF_COFFEE_BEANS - 16
+            COFFEE_MACHINE_INCOME = COFFEE_MACHINE_INCOME + 4
+            AMOUNT_OF_DISPOSABLE_CUPS -= 1
+
+    elif user_input == 2:
+        if AMOUNT_OF_WATER < 350:
+            print("Sorry, not enough water!")
+        elif AMOUNT_OF_COFFEE_BEANS < 20:
+            print("Sorry, not enough coffee beans!")
+        elif AMOUNT_OF_MILK < 75:
+            print("Sorry, not enough milk!")
+        elif AMOUNT_OF_DISPOSABLE_CUPS < 1:
+            print("Sorry, not enough disposable cups!")
+        else:
+            print("I have enough resources, making you a coffee!")
+            AMOUNT_OF_WATER = AMOUNT_OF_WATER - 350
+            AMOUNT_OF_MILK = AMOUNT_OF_MILK - 75
+            AMOUNT_OF_COFFEE_BEANS = AMOUNT_OF_COFFEE_BEANS - 20
+            COFFEE_MACHINE_INCOME = COFFEE_MACHINE_INCOME + 7
+            AMOUNT_OF_DISPOSABLE_CUPS -= 1
+
+    elif user_input == 3:
+        if AMOUNT_OF_WATER < 200:
+            print("Sorry, not enough water!")
+        elif AMOUNT_OF_COFFEE_BEANS < 12:
+            print("Sorry, not enough coffee beans!")
+        elif AMOUNT_OF_MILK < 100:
+            print("Sorry, not enough milk!")
+        elif AMOUNT_OF_DISPOSABLE_CUPS < 1:
+            print("Sorry, not enough disposable cups!")
+        else:
+            print("I have enough resources, making you a coffee!")
+            AMOUNT_OF_WATER = AMOUNT_OF_WATER - 200
+            AMOUNT_OF_MILK = AMOUNT_OF_MILK - 100
+            AMOUNT_OF_COFFEE_BEANS = AMOUNT_OF_COFFEE_BEANS - 12
+            COFFEE_MACHINE_INCOME = COFFEE_MACHINE_INCOME + 6
+            AMOUNT_OF_DISPOSABLE_CUPS -= 1
 
 
 def state_of_coffee_machine():
@@ -29,28 +83,21 @@ def coffee_machine_buy():
     global COFFEE_MACHINE_INCOME
     global AMOUNT_OF_MILK
     global AMOUNT_OF_DISPOSABLE_CUPS
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - capppuccino:")
-    user_input = int(input())
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - capppuccino, back - to main:")
+    user_input = input()
 
-    if user_input == 1:
-        AMOUNT_OF_WATER = AMOUNT_OF_WATER - 250
-        AMOUNT_OF_COFFEE_BEANS = AMOUNT_OF_COFFEE_BEANS - 16
-        COFFEE_MACHINE_INCOME = COFFEE_MACHINE_INCOME + 4
-        AMOUNT_OF_DISPOSABLE_CUPS -= 1
+    if user_input == "1":
+        check_coffee_resources_and_act(1)
 
-    elif user_input == 2:
-        AMOUNT_OF_WATER = AMOUNT_OF_WATER - 350
-        AMOUNT_OF_MILK = AMOUNT_OF_MILK - 75
-        AMOUNT_OF_COFFEE_BEANS = AMOUNT_OF_COFFEE_BEANS - 20
-        COFFEE_MACHINE_INCOME = COFFEE_MACHINE_INCOME + 7
-        AMOUNT_OF_DISPOSABLE_CUPS -= 1
+    elif user_input == "2":
+        check_coffee_resources_and_act(2)
 
-    elif user_input == 3:
-        AMOUNT_OF_WATER = AMOUNT_OF_WATER - 200
-        AMOUNT_OF_MILK = AMOUNT_OF_MILK - 100
-        AMOUNT_OF_COFFEE_BEANS = AMOUNT_OF_COFFEE_BEANS - 12
-        COFFEE_MACHINE_INCOME = COFFEE_MACHINE_INCOME + 6
-        AMOUNT_OF_DISPOSABLE_CUPS -= 1
+    elif user_input == "3":
+        check_coffee_resources_and_act(3)
+
+    elif user_input == "back":
+        coffee_machine_operation(input("Write action (buy, fill, take, remaining, exit):"))
+        exit(0)
 
 
 def coffee_machine_fill():
@@ -81,24 +128,39 @@ def coffee_machine_take():
     COFFEE_MACHINE_INCOME = 0
 
 
-def coffee_machine_operation():
-    print("Write action (buy, fill, take, remaining, exit):")
-    user_input = input()
+def coffee_machine_operation(user_input):
+    if user_input == "exit":
+        exit(0)
+    else:
+        while user_input == "buy" or user_input == "fill" or user_input == "take" or user_input == "remaining":
+            if user_input == "buy":
+                coffee_machine_buy()
+                print()
+                user_input = input("Write action (buy, fill, take, remaining, exit):")
 
-    if user_input == "buy":
-        coffee_machine_buy()
-        print()
-        state_of_coffee_machine()
-    elif user_input == "fill":
-        coffee_machine_fill()
-        print()
-        state_of_coffee_machine()
-    elif user_input == "take":
-        coffee_machine_take()
-        print()
-        state_of_coffee_machine()
-    elif user_input == "remaining":
-        state_of_coffee_machine()
+            elif user_input == "fill":
+                coffee_machine_fill()
+                print()
+                user_input = input("Write action (buy, fill, take, remaining, exit):")
+
+            elif user_input == "take":
+                coffee_machine_take()
+                print()
+                user_input = input("Write action (buy, fill, take, remaining, exit):")
+
+            elif user_input == "remaining":
+                state_of_coffee_machine()
+                print()
+                user_input = input("Write action (buy, fill, take, remaining, exit):")
+
+            elif user_input == "exit":
+                break
 
 
-coffee_machine()
+coffee_machine_operation(input("Write action (buy, fill, take, remaining, exit):"))
+print()
+
+
+#if __name__ == "__main__":
+#    coffee_machine()
+
